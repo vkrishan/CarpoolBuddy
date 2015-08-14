f33db6311683f85c4f54bec32213392e8986e59a
vaibhav krishan <vaibhavx.krishan@intel.com>
2015-08-14 14:31:41 -0700

    Code Upload

---
+++ app/src/main/java/com/carpool/vkrishax/carpool/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.carpool.vkrishax.carpool;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class LoginActivity extends Activity {
+
+    TextView Register;
+    Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initializeViews();
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int2 = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(int2);
+            }
+        });
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int1 = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(int1);
+            }
+        });
+    }
+
+    public void initializeViews(){
+
+    Register = (TextView)findViewById(R.id.textView2);
+    Login =  (Button)findViewById(R.id.Loginbutton);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
